3d9f2c943051b49c799d89b21373e21014d5e211
priyanshik0902 <latakotian09@gmail.com>
2024-04-10 17:29:32 +0530

    easy

---
+++ Day02_205. Isomorphic Strings.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <unordered_map>
+
+class Solution {
+public:
+    bool isIsomorphic(std::string s, std::string t) {
+        std::unordered_map<char, char> s_to_t;
+        std::unordered_map<char, char> t_to_s;
+
+
+        for (int i = 0; i < s.length(); ++i)
+         {
+            char char_s = s[i];
+            char char_t = t[i];
+            if (s_to_t.find(char_s) != s_to_t.end() && s_to_t[char_s] != char_t)
+             {
+                return false;
+             }
+
+            if (t_to_s.find(char_t) != t_to_s.end() && t_to_s[char_t] != char_s)
+            {
+                return false;
+            }
+            s_to_t[char_s] = char_t;
+            t_to_s[char_t] = char_s;
+        }
+        return true;
+    }
+};
